a5334233f902e07815297e58e4639761598289a0
Theresa Nheap <theresa.nheap@gmail.com>
2023-08-06 14:48:30 -0700

    Create

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.storelocator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.storelocator to javafx.fxml;
+    exports com.example.storelocator;
+}
