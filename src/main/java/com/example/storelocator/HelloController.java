ae36459dfb846e4b76d3e17925bc154425ddbd13
Theresa Nheap <theresa.nheap@gmail.com>
2023-08-06 15:59:48 -0700

    Update HelloController.java

--- src/main/java/com/example/storelocator/HelloController.java
+++ src/main/java/com/example/storelocator/HelloController.java
@@ -2,13 +2,18 @@ package com.example.storelocator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField nameInput;
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String name = nameInput.getText();
+        welcomeText.setText("Hello, " + name + "!");
     }
 }
